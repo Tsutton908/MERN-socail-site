dd03058305492a5123b840a4ce75e44e0b86292b
William Sutton <tsutton908@gmail.com>
2021-06-07 16:15:20 -0400

    backend routes and models complete

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send('API Running'))
+
+// Define Routes
+//use(endpoint, retrieval location)
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
